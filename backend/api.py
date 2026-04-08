e62765a8370382f9df65290355fc7917379cbd60
Mcx13 <mariclarabarreto@hotmail.com>
2026-04-08 20:42:57 -0300

    integração api

---
+++ backend/api.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from models import Usuario
+from services.ocorrencias import processar_ocorrencia
+
+app = FastAPI()
+
+class OcorrenciaEntrada(BaseModel):
+    nome: str
+    local: str
+    veiculo: str
+    infracao: str
+
+def gerar_palavra_chave(infracao):
+    if "garagem" in infracao:
+        return "garagem"
+    elif "idoso" in infracao:
+        return "idoso"
+    elif "deficiente" in infracao:
+        return "deficiente"
+    elif "faixa" in infracao:
+        return "faixa"
+    elif "carga e descarga" in infracao:
+        return "carga_descarga"
+    elif "onibus" in infracao:
+        return "onibus"
+    elif "esquina" in infracao:
+        return "esquina"
+    elif "rampa" in infracao:
+        return "rampa"
+    else:
+        return "irregularidade"
+
+def gerar_nome_arquivo(primo_nome, palavra_chave):
+    agora = datetime.now()
+    data_formatada = agora.strftime("%d%m%y_%H%M%S")
+    return f"{primo_nome}_{palavra_chave}_{data_formatada}.jpg"
+
+
+@app.post("/ocorrencia")
+def criar_ocorrencia(dados: OcorrenciaEntrada):
+    primeiro_nome = dados.nome.split()[0].capitalize()
+    usuario = Usuario(primeiro_nome)
+
+    palavra_chave = gerar_palavra_chave(dados.infracao)
+    foto = gerar_nome_arquivo(primeiro_nome, palavra_chave)
+
+    resultado = processar_ocorrencia(
+        usuario=usuario,
+        local=dados.local,
+        veiculo=dados.veiculo,
+        foto=foto
+    )
+
+    return resultado
